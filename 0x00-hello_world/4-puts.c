f70509738471ad1321d7c693b3db483844820342
Marwane <khmarwane10@gmail.com>
2023-06-15 14:18:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/*
+ * Main :Program starts with the entry point. In C it’s the main function
+ *	 From the main function you can call other functions
+ *	 When main returns, the program stops 
+
+ * Return: Ends the function and returns to the calling function
+*/ 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
