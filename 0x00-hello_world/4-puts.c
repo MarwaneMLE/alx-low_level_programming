1a039409596e0cbd794acb7ea4f1da76ce80901f
Marwane <khmarwane10@gmail.com>
2023-06-15 15:27:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * Main-A program print a line with puts function
+ * Descripction: Main - A program print a line with puts function
  * Return: 0 is success
  */ 
 int main(void)
