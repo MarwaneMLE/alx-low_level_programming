f5ce55aeb055bfe9e1cb295d9fbeb0fe48c4d0f6
Marwane <khmarwane10@gmail.com>
2023-06-17 15:06:30 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - in this program prints all possible combinations of two two-digit numbers.
+ * Return: (0) if success
+ */
+int main(void)
+{
+	int i;
+	int j,
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
