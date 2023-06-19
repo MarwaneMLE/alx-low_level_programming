be5b7f36075292b5180e6a05a911e77092e56085
Marwane <khmarwane10@gmail.com>
2023-06-19 15:46:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * Description:
+ * main: program that prints exactly and that piece of art is useful\" - Dora Korpar,\2015-10-19
+ * Return: 1 if success
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return 1;
+}
