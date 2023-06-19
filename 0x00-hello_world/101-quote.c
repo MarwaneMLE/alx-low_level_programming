17cd3a292ad2f0c66e52d1e95da0a69d1d510f83
Marwane <khmarwane10@gmail.com>
2023-06-19 15:59:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include <unistd.h>
 /**
  * Description: main- program that prints specific message.
  * Return: 1 if success.
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
