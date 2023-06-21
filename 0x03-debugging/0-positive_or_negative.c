af76ccb0d976052117e0949bf4dcad70c28e564a
Marwane <khmarwane10@gmail.com>
2023-06-21 10:31:56 -0700

    main task

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Determines if generated number is positive, null or negative.
+ * Return: (0) is success.
+ */
+int main(void)
+{
+	int n;
+	/* Use current time as seed for random generator */
+	srand(time(0));
+	/* rand(): generates a random number from "0" to RAND_MAX */
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
