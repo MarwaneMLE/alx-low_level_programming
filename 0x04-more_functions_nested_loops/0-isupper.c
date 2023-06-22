07ad7d081e9c1e350f193d95cc699b80a281d739
Marwane <khmarwane10@gmail.com>
2023-06-22 03:11:06 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ /**
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 1 on success else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c >= 'Z')
+		return (1);
+	else
+		return (0);
+}
