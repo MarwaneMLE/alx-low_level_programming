3d82f730ff4525173fd3073bb724f3406f231eac
Marwane <khmarwane10@gmail.com>
2023-06-26 04:16:15 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen: return length of string
+ * @s: string to count length
+ * Return the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
