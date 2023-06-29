9f905b287fef3751298c35f71f715909d8288ba7
Marwane <khmarwane10@gmail.com>
2023-06-29 14:25:34 -0700

    reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,20 @@
 #include "main.h"
 /**
- * _strncpy- Copy a string
- * @dest: input string
- * @src: input string
- * @n: input
- * Return: dest
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: (0) 
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int i;
 
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[j] = src[j];
-		j++;
-	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = *(src + i);
+	for ( ; i < n; i++)
+		*(dest + i) = '\0';
 	return (dest);
-}   
+}
