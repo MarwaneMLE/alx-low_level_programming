f6dd78867cbde56bac4118a989a64cf49e6a51ae
Marwane <khmarwane10@gmail.com>
2023-07-03 14:27:04 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,21 +9,22 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-        int a, b;
+	int a, b;
 
-        for (a = 0 ; *(s + a) ; a++)
-        {
-                for (b = 0 ; *(accept + b) ; b++)
-                {
-                        if (*(accept + b) == s[a])
-                        {
-                                return (s + a);
-                        }
-                }
-                if (*(accept + b) == s[a])
-                {
-                        return (s + a);
-                }
-        }
-        return (0);
+	for (a = 0 ; *(s + a) ; a++)
+	{
+		for (b = 0 ; *(accept + b) ; b++)
+		{
+			if (*(accept + b) == s[a])
+			{
+				return (s + a);
+			}
+		}
+                        
+                if (*(accept + b) == s[a])	
+		{
+			return (s + a);
+		}
+	}
+	return (0);
 }
