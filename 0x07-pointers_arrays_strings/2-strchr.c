8ecd373b493746dd8ab7a848299666c9b51a218e
Marwane <khmarwane10@gmail.com>
2023-07-03 10:58:14 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,18 @@
 
 char *_strchr(char *s, char c)
 {
-        while (*s != '\0')
-        {
-                if (*s == c)
-                {
-                        return (s);
-                }
-                else
-                        s++;
-        }
-        if (*s == c)
-                return (s);
-        else
-                return (0);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (0);
 }
