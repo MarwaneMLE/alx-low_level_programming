6a8f6280e22162ea4ec0e248c1e10f45f7ae05ef
Marwane <khmarwane10@gmail.com>
2023-07-31 14:55:44 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a linked list
+ * listint_len - prints all elements of a linked list
  * @h: linked list of type listint_t to traverse
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t number = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		number++;
 		h = h->next;
 	}
