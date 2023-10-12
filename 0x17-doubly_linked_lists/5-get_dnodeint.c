52cc760dd4bf27bc85c85730ae6e3cb879b7a84e
Marwane <khmarwane10@gmail.com>
2023-10-12 16:37:42 -0700

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,24 +3,23 @@
 /**
  * get_dnodeint_at_index - function that returns the nth node
  * of a dlistint_t linked list.
- * @head: 
+ * @head: head of linked list
  * @index: the index is the index of the node, starting from 0
  *
- * Return: returns the nth node
- * 	    if node does not exit NULL
+ * Return: returns the nth node, if node does not exit NULL
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
-	i = 0;
 
+	i = 0;
 	/* Check if the head is NULL */
-	 if (head == NULL)
-		 return (NULL);
+	if (head == NULL)
+		return (NULL);
 
 	 /* While the head is not NULL */
-	 while (head != NULL)
+	while (head != NULL)
 	{
 		/* Initialize the search here */
 		if (i == index)
@@ -30,6 +29,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		/* Make head point to the next node if the index is not found */
 		head = head->next;
 	}
-	
-	 return (head);
+	return (head);
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,23 +4,23 @@
  * sum_dlistint -  function that returns the sum of all the data
  *(n) of a dlistint_t linked list.
  * @head:Pointer to the head of the list.
- * Return: 0 if  list is empty
- * 	   sum if not
+ *
+ * Return: 0 if list is empty, sum if not
  */
 
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *current;
+	dlistint_t *next;
 	int sum = 0;
 
-	/*  Iterate through the linked list while the pointer 'head' is not NULL */
-	while (!head)
+	current = head;
+
+	while (current != NULL)
 	{
-		/*  Iterate through the linked list while the pointer 'head' is not NULL  */
-		sum += head->n;
-
-		/* Move to the next node in the list */
-		head = head->next;
+		next = current->next;
+		sum += current->n;
+		current = next;
 	}
-
-	return sum;
+	return (sum);
 }
