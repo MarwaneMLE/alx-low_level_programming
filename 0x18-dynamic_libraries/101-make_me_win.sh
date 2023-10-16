39b28494345bb13d2b1ed93cad8c7ebb68352ba3
Marwane <khmarwane10@gmail.com>
2023-10-16 16:02:26 -0700

    C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MarwaneMLE/alx-low_level_programming/0x18-dynamic_librarieslibgiga.so
+wget -P /tmp https://github.com/MarwaneMLE/"\n"
+alx-low_level_programming/0x18-dynamic_librarieslibgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
